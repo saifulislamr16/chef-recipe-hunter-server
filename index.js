374dc8142b732ffbcd221ecec876580ca25c73ad
saifulislamr16 <saifulislamr16@gmail.com>
2023-06-02 16:12:53 +0600

    chefs data added

--- index.js
+++ index.js
@@ -5,11 +5,15 @@ const port = process.env.PORT || 3000;
  
 app.use(cors())
 
+const chefs = require('./chefData.json')
+
 app.get('/', (req, res) =>{
     res.send('Chef hunter server is running')
 })
 
-
+app.get('/chefs', (req, res) =>{
+    res.send(chefs)
+})
 
 app.listen(port, () => {
     console.log(`Chef Hunter API is listening on port: ${port}`)
